e92f7af82d4df1d56b5f61e8ea179b2492f917b9
James Battye <jamesbattye98@gmail.com>
2024-05-21 11:52:14 +0900

    improved iframe handling

---
+++ .changeset/tough-seahorses-obey.md
@@ -0,0 +1,5 @@
+---
+'@yes-chef/earcandy': patch
+---
+
+improved iframe handling
--- src/videoPlayer.js
+++ src/videoPlayer.js
@@ -3,30 +3,52 @@ import LazyLoad from 'vanilla-lazyload';
 let videoPlayers = [];
 
 function pauseVideos() {
-  videoPlayers.forEach((e) => {
-    e.pause();
+  videoPlayers.forEach((player) => {
+    player.pause();
   });
 }
 
-new LazyLoad(
-  {
-    callback_loaded: function (e) {
-      if (e.tagName === 'IFRAME') {
-        videoPlayers.push(new Vimeo.Player(e, e.getAttribute('video-id')));
-      }
+function initVideoPlayers() {
+  new LazyLoad(
+    {
+      elements_selector: 'iframe',
+      callback_loaded: function (iframe) {
+        if (iframe.tagName === 'IFRAME') {
+          videoPlayers.push(new Vimeo.Player(iframe, { id: iframe.getAttribute('video-id') }));
+          console.log('loaded');
+        }
+      },
     },
-  },
-  document.querySelectorAll('iframe')
-);
+    document.querySelectorAll('.video-popup_wrap.is-open iframe')
+  );
+}
 
-setTimeout(() => {
-  const slider = ycAttributes.sliders['videos-0'];
-
-  slider.on('slideChange', () => {
-    pauseVideos();
+// Setting up MutationObserver to observe changes in the class attribute of the popup
+const popupWrapper = document.querySelector('.video-popup_wrap');
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+      const { target } = mutation;
+      if (target.classList.contains('is-open')) {
+        initVideoPlayers();
+      } else {
+        pauseVideos();
+      }
+    }
   });
-}, 250);
+});
+
+// Start observing
+observer.observe(popupWrapper, { attributes: true });
 
 document.querySelector('.video-popup_close-icon').addEventListener('click', () => {
   pauseVideos();
 });
+
+// Assuming ycAttributes and sliders are already defined elsewhere in your script
+setTimeout(() => {
+  const slider = ycAttributes.sliders['videos-0'];
+  slider.on('slideChange', () => {
+    pauseVideos();
+  });
+}, 250);
